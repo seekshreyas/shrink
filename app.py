8a883b1c51fc61754889fbe79eb4bc939e0ff4a9
Shreyas <seekshreyas@gmail.com>
2013-12-12 21:50:35 -0800

    redirection working and homepage rendering

--- app.py
+++ app.py
@@ -4,18 +4,21 @@ import shelve
 import flask
 from flask import request
 # from os import environ
-# from shrinkdb import db
+from shrinkdb import db
+from shrinkdb import Links
+from shrinkdb import Bundles
 import json
 import re
 
 app = flask.Flask(__name__)
 app.debug = True
 
-db = shelve.open("shorten.db")
+# db = shelve.open("shorten.db")
 
 # db = {}
 
 
+db.create_all()
 
 
 
@@ -25,15 +28,26 @@ db = shelve.open("shorten.db")
 # Only supports the GET method, returns a homepage represented as HTML
 ###
 @app.route('/', methods=['GET'])
+def root():
+    """Redirectos to homepage"""
+    destination = '/home'
+    app.logger.debug("Redirecting to " + destination)
+    return flask.redirect(destination)
+
+
+
+@app.route('/home', methods=['GET'])
 def home():
-    """Builds a template based on a GET request, with some default
-    arguements"""
-    index_title = request.args.get("title", "i253")
-    hello_name = request.args.get("name", "Jim")
-    return flask.render_template(
-            'index.html',
-            title=index_title,
-            name=hello_name)
+    """Render the hompepage"""
+    # index_title = request.args.get("title", "i253")
+    # hello_name = request.args.get("name", "Jim")
+    bund = Bundles.query.all()
+    links = Links.query.all()
+    app.logger.debug("Bundles =>" + str(bund))
+    app.logger.debug("Links =>" + str(links))
+
+
+    return flask.render_template('index.html',responselinks=links, responsebundles=bund)
 
 
 
@@ -42,67 +56,61 @@ def home():
 # GET method will redirect to the resource stored by PUT, by default: Wikipedia.org
 # POST/PUT method will update the redirect destination
 ###
-@app.route('/shorts', methods=['GET'])
-def short():
-    """
-    Show the form page
-    """
-    return flask.render_template('shorten.html')
+# @app.route('/shorts', methods=['GET'])
+# def short():
+#     """
+#     Show the form page
+#     """
+#     return flask.render_template('shorten.html')
 
 
 
-@app.route('/shorts/<surl>', methods=['GET'])
+@app.route('/s/<surl>', methods=['GET'])
 def short_get(surl):
     """
     Redirect to the shortened url
     """
-    shorturl = str(surl)
+    surl = str(surl)
 
-    msg = {}
-    if db.has_key(shorturl):
-        app.logger.debug("Redirect to =>" + db[shorturl])
+    record = Links.query.filter_by(shorturl=surl).first_or_404()
 
-        return flask.redirect("http://" + db[shorturl])
-    else:
-        msg['type'] = 'ERROR'
-        msg['txt'] = 'Short url doesnt exist'
+    app.logger.debug("Request : " + str(record.shorturl) + " => " + str(record.longurl))
 
-        return flask.render_template('response.html',
-                                    msgtype=msg['type'],
-                                    msgtxt=msg['txt'] )
-
-
-
-@app.route("/shorts", methods=['PUT', 'POST'])
-def short_put():
-    """
-    create a shortened url for the link
-    """
-    shorturl = str(request.form['s'])
-    longurl = str(request.form['l'])
+    return flask.redirect("http://" + record.longurl)
 
 
 
 
-    msg = {}
-    if db.has_key(shorturl):
-        msg['type'] = 'ERROR'
-        msg['txt'] = 'Short URL already exists'
+# # @app.route("/shorts", methods=['PUT', 'POST'])
+# # def short_put():
+# #     """
+# #     create a shortened url for the link
+# #     """
+# #     shorturl = str(request.form['s'])
+# #     longurl = str(request.form['l'])
 
 
-    else:
 
-        # regex courtesy: http://stackoverflow.com/questions/11242258/strip-url-python
-        longurl = re.match(r'(?:\w*://)?(?:.*\.)?([a-zA-Z-1-9]*\.[a-zA-Z]{1,}).*', longurl).groups()[0]
 
-        db[shorturl] = longurl
+#     msg = {}
+#     if db.has_key(shorturl):
+#         msg['type'] = 'ERROR'
+#         msg['txt'] = 'Short URL already exists'
 
-        msg['type'] = 'Success'
-        msg['txt'] = db[shorturl] + " => " + shorturl
 
-    return flask.render_template('response.html',
-                                    msgtype=msg['type'],
-                                    msgtxt=msg['txt'] )
+#     else:
+
+#         # regex courtesy: http://stackoverflow.com/questions/11242258/strip-url-python
+#         longurl = re.match(r'(?:\w*://)?(?:.*\.)?([a-zA-Z-1-9]*\.[a-zA-Z]{1,}).*', longurl).groups()[0]
+
+#         db[shorturl] = longurl
+
+#         msg['type'] = 'Success'
+#         msg['txt'] = db[shorturl] + " => " + shorturl
+
+#     return flask.render_template('response.html',
+#                                     msgtype=msg['type'],
+#                                     msgtxt=msg['txt'] )
 
 
 
